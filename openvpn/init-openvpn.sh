3331c310f1be00dd0ecacc0e6ede2cb71cd84566
yosefrow <yosefrow@gmail.com>
2020-03-11 07:41:49 +0200

    fix init script

--- openvpn/init-openvpn.sh
+++ openvpn/init-openvpn.sh
@@ -8,8 +8,18 @@ cd $(dirname ${0})
 )
 source .env
 
+echo "Create Data dir"
+echo "============================="
+sudo mkdir -p ${OPENVPN_DATA_DIR_EXTERNAL}
+
+echo "Fix ownership"
+echo "============================="
+sudo chown -R $(whoami): ${OPENVPN_DATA_DIR_EXTERNAL}
+
 echo Initialize the configuration files and certificates
 echo =============================
 docker-compose run --rm openvpn ovpn_genconfig -u udp://${SERVER_PUBLIC_URL}
 docker-compose run --rm openvpn ovpn_initpki
+
+./build.sh
 
