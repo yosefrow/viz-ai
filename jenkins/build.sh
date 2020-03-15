f5349ba2d26b4fa3155ffaf94ee28d85c9e0c039
yosefrow <yosefrow@gmail.com>
2020-03-15 17:46:02 +0000

    jenkins second draft

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd $(dirname ${0})
 
@@ -8,14 +8,29 @@ cd $(dirname ${0})
 )
 source .env
 
-echo "Create Data dir"
-echo "============================="
+function title() {
+    local text="${@}"
+    echo 
+    echo "${text}"
+    echo "============================="
+}
+
+IMAGE_UID=$(docker run --rm -it ${DOCKER_IMAGE} id -u | tail -n 1 | tr -d '\r')
+
+title "Create Data dir"
 sudo mkdir -p ${JENKINS_DATA_DIR_EXTERNAL}
 
-echo "Fix ownership"
-echo "============================="
-sudo chown -R $(whoami): ${JENKINS_DATA_DIR_EXTERNAL}
+title "Fix ownership"
+sudo chown -R $IMAGE_UID ${JENKINS_DATA_DIR_EXTERNAL}
 
-echo "Start server process"
-echo "============================="
-docker-compose up -d jenkins
+title "Restart server process"
+docker-compose down || echo 'Info: could not docker-compose down'
+docker-compose up -d ${SERVICE_NAME}
+
+title "Waiting for ${SERVICE_NAME} service..."
+while true; do
+    curl -sL http://localhost:${JENKINS_HTTP_PORT_EXTERNAL} -o /dev/null && break
+    sleep 2
+done
+
+title "Done."
