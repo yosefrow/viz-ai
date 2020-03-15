8a56d1954b8134de01da5d029e9283f75981b6ed
yosefrow <yosefrow@gmail.com>
2020-03-15 17:06:14 +0000

    first draft of nginx service

---
+++ nginx/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+cd $(dirname ${0})
+
+[ -f .env ] || (
+    echo ".env not found! Copying from .env.example" && 
+    cp -p .env.example .env
+)
+source .env
+
+function title() {
+    local text="${@}"
+    echo 
+    echo "${text}"
+    echo "============================="
+}
+
+title "Restart server process"
+docker-compose down || echo 'Info: could not docker-compose down'
+docker-compose up -d ${SERVICE_NAME}
+
+title "Waiting for ${SERVICE_NAME} service..."
+while true; do
+    curl -sL http://localhost:${NGINX_HTTP_PORT_EXTERNAL} -o /dev/null && break
+    sleep 2
+done
+
+title "Done."
